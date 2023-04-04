35823e6f8ca59cd53769466ee474eab9cabb55de
Marcos Minond <minond.marcos@gmail.com>
2023-04-04 16:00:31 -0600

    New route and readme

---
+++ README.md
@@ -0,0 +1,4 @@
+# Flask Demo Application
+
+This is a small demo Flask application. You can access it with from this URL:
+https://flask-demo-69yf.onrender.com
--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 import os
+import time
 import flask
 
 app = flask.Flask(__name__)
 
 @app.route("/")
-def index():
+def index_page():
     return "Hello, World"
+
+# http://127.0.0.1:5000/time
+@app.route("/time")
+def time_page():
+    return time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.gmtime())
 
 app.run(port=os.environ.get("PORT", 8080))
